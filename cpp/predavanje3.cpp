6da9d367f6ca643a95edb9799ed9f57c89cb3042
dervish13 <rajkod@hotmail.com>
2018-12-04 22:55:00 +0100

    28.11.2018

--- cpp/predavanje3.cpp
+++ cpp/predavanje3.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
 #include <vector>
-
+#include <map>
 using namespace std;
+class LifeForm
+{
+	public:
+	LifeForm( const string &name="")
+	{
+		_name=name;
+	}
+	void speak()
+	{
+		cout<< "Hello, I am "<< _name <<endl;
+	}
+	private:
+	string _name;
+};
 int main()
 {
 	vector <int> brojevi;
@@ -24,8 +38,25 @@ int main()
 		cout<< *it<<endl;
 
 	}
+	vector <LifeForm> niz_bica;
+	LifeForm a();
+	LifeForm l("Fadl Stihl");
+	niz_bica.push_back(l);
+	niz_bica.emplace_back("Fatima");
+	auto it= niz_bica.begin();
+	it +=2;
+	auto mujo=niz_bica.emplace(it,"Mujo");
+	for (auto &lf : niz_bica)
+	{
+		lf.speak();
+	}
+	mujo->speak();
 
-
+	map<string,LifeForm> popis;
+	popis["t5t11"]=LifeForm("Rajko");
+	popis.emplace("ggg","Cvrc");
+	popis["t5t11"].speak();
+	popis["ggg"].speak();
 	return 0;
 
 }
